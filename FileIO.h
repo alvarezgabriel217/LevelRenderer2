05ca2d939bfc666461e1df126b324ed238c319a0
alvarezgabriel217 <alvarezgabriel217@gmail.com>
2022-12-06 23:19:12 -0500

    Setup2

---
+++ FileIO.h
@@ -0,0 +1,86 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "Gateware.h"
+
+std::ifstream input;
+std::string filename = "../GameLevel.txt";
+
+std::vector<GW::MATH::GMATRIXF> world;
+std::vector<std::string> names;
+
+void openFile()
+{
+	input.open(filename);
+
+	if (input.is_open())
+	{
+		while (!input.eof())
+		{
+			//char buffer[256];
+			std::string buffer2;
+			std::getline(input, buffer2);
+			//input.getline(buffer, 256);
+			if (std::strcmp("MESH", buffer2.c_str()) == 0)
+			{
+				//input.getline(buffer, 256);
+				//input.getline(buffer, 256);
+
+				std::getline(input, buffer2);
+
+				names.push_back(buffer2.substr(0, buffer2.find('.')) + ".h2b");
+
+				GW::MATH::GVECTORF rows[4];
+
+				for (int i = 0; i < 4; i++)
+				{
+					std::getline(input, buffer2);
+					buffer2 = buffer2.substr(buffer2.find('(') + 1, buffer2.find(')') - buffer2.find('(') - 1);
+
+					rows[i].x = atof(buffer2.c_str());
+
+					buffer2 = buffer2.substr(buffer2.find(',') + 1);
+
+					rows[i].y = atof(buffer2.c_str());
+
+					buffer2 = buffer2.substr(buffer2.find(',') + 1);
+
+					rows[i].z = atof(buffer2.c_str());
+
+					buffer2 = buffer2.substr(buffer2.find(',') + 1);
+
+					rows[i].w = atof(buffer2.c_str());
+				}
+
+				world.push_back(GW::MATH::GMATRIXF{ rows[0], rows[1], rows[2], rows[3] });
+			}
+		}
+		for (int i = 0; i < world.size(); i++)
+		{
+			std::cout << names[i] << std::endl;
+			std::cout << world[i].row1.x << ", ";
+			std::cout << world[i].row1.y << ", ";
+			std::cout << world[i].row1.z << ", ";
+			std::cout << world[i].row1.w << std::endl;
+			std::cout << world[i].row2.x << ", ";
+			std::cout << world[i].row2.y << ", ";
+			std::cout << world[i].row2.z << ", ";
+			std::cout << world[i].row2.w << std::endl;
+			std::cout << world[i].row3.x << ", ";
+			std::cout << world[i].row3.y << ", ";
+			std::cout << world[i].row3.z << ", ";
+			std::cout << world[i].row3.w << std::endl;
+			std::cout << world[i].row4.x << ", ";
+			std::cout << world[i].row4.y << ", ";
+			std::cout << world[i].row4.z << ", ";
+			std::cout << world[i].row4.w << std::endl << std::endl;
+		}
+
+		input.close();
+	}
+	else
+	{
+		std::cout << "Filename not found";
+	}
+}
---
+++ Level_Data.h
@@ -0,0 +1,14 @@
+#pragma once
+//#include "../Gateware.h"
+#include "../h2bParser.h"
+
+class Level_Data 
+{
+	//H2B::VERTEX verts[];
+	H2B::MATERIAL mats[];
+	//GW::MATH::GVECTORF verts[];
+	//GW::MATH::GVECTORF verts[];
+	//GW::MATH::GVECTORF verts[];
+
+
+};
